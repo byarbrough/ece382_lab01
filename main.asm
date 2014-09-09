b6d1dac0b1962ec305977fbd57533e434df8d5ea
Brian Yarbrough <C16Brian.Yarbrough@usafa.edu>
2014-09-09 13:35:07 -0600

    almost required functionality

--- main.asm
+++ main.asm
@@ -17,7 +17,9 @@ ops:	.byte	0x14, 0x11, 0x12, 0x55
 
 fOp:	.equ	r5	;register for the first number
 sOp:	.equ	r6	;register for the second number
-tsk:	.equ	r7	;register for the instruction to be interested
+tsk:	.equ	r7	;register for the instruction
+point:	.equ	r8	;register for pointer
+mem:	.equ	r9	;register for memory location
 
 ;--------------------------------------------------------------------------------
 
@@ -29,6 +31,38 @@ StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
                                             ; Main loop here
 ;-------------------------------------------------------------------------------
 
+		mov.b	#ops, point	;point first op at string of numbers
+		mov		#0x200, mem	;starting locaiton for memroy
+
+cleared	mov.b	@point+, fOp ;fOp = value at point
+again	mov.b	@point+, tsk ;tsk = the next number
+
+chkEnd	cmp.b	#0x55, tsk	;check for end command
+		jz		end			;terminate program
+
+chkClr	cmp.b	#0x44, tsk	;check for clear command
+		jz		clear
+;if not ended or clear, do arithmitic
+
+		mov.b	@point+, sOp	;load the second operand into register
+
+chkAdd	cmp.b	#0x11, tsk	;compare the opperand
+		jnz		chkSub		;jump if not add
+		add.b	fOp, sOp	;numbers are added
+		jmp		store		;store sum
+
+chkSub	sub.b	fOp, sOp
+		jmp 	store		;store difference
+
+
+end		jmp		end	;infinite loop
+
+;submethods
+clear	jmp		cleared	;resume program
+
+store	mov.b	sOp, mem	;store result in memory
+		inc		mem
+		jmp		again	;resume program with opcode
 
 ;-------------------------------------------------------------------------------
 ;           Stack Pointer definition
